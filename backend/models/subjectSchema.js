09cd64c33b2d82bd442813841aabaa5418df85e6
sanju <sanjuraj57421@gmail.com>
2025-01-18 00:24:38 +0530

    add subjectSchema

---
+++ backend/models/subjectSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+// Subject Schema
+const subjectSchema = new mongoose.Schema(
+    {
+        subName: { type: String, required: true },
+        subCode: { type: String, required: true },
+        sessions: { type: String, required: true },
+        sclassName: { type: mongoose.Schema.Types.ObjectId, ref: 'sclass', required: true },
+        school: { type: mongoose.Schema.Types.ObjectId, ref: 'admin' },
+        teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'teacher' }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('subject', subjectSchema);
